ebd1eaae8a6cb3cf0e9ee91735556276358da6f0
Youssef-Bu <youssef.bouchikhiu@epsi.com>
2025-01-22 15:53:46 +0100

    WIP

--- sidecar/pull.sh
+++ sidecar/pull.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+if [ -d .git ]; then
+  echo "Dépôt Git trouvé. Synchronisation..."
+else
+  echo "Erreur : Ce répertoire n'est pas un dépôt Git."
+  exit 1
+fi
+
+# Boucle pour effectuer le git pull toutes les 10 secondes
 while true; do
   git pull origin main
   sleep 10
